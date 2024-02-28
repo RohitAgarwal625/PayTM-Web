28366ffed61b00035d71f8960aa03033163352e4
Rohit <thisisrohit625@gmail.com>
2024-02-28 06:37:17 +0530

    signup commit for testing to prev commits

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -65,7 +65,7 @@ export const Signup = () => {
               navigate("/signin")
             }
 
-            catch(error){ console.error("Signup Failed testing for 2nd commit",error)
+            catch(error){ console.error("Signup Failed ",error)
       //       if (error.response && error.response.data && error.response.data.message) {{
       //   setEmailIncorrect(error.response.data.message);}
       // } else {
